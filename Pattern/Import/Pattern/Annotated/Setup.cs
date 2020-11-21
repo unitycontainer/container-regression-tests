e8f437c6a94f4477bbd94b352145885381b1130a
Eugene Sadovoi <evgeni@eniks.com>
2020-11-21 10:00:42 -0800

    Cleanup

--- Pattern/Import/Pattern/Annotated/Setup.cs
+++ Pattern/Import/Pattern/Annotated/Setup.cs
@@ -1,22 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-#if UNITY_V4
-using Microsoft.Practices.Unity;
-#else
-using Unity;
-#endif
 
 namespace Regression.Annotated
 {
     public abstract partial class Pattern : PatternBase
     {
-        [TestMethod]
-        public void Baseline()
-        {
-            var type = GetType("BaselineTestType");
-            var instance = Container.Resolve(type, null) as PatternBaseType;
-
-            Assert.IsNotNull(instance);
-            Assert.IsNull(instance.Value);
-        }
     }
 }
